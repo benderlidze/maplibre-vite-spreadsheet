43728736175421a6b8277f8df2ebe5475698e9ce
Serg <benderlio@gmail.com>
2025-03-28 20:02:40 +0100

    Updates

---
+++ assets/about-Bf0uWzxE.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-pzfDBdG-.js";const s=function(){return t.jsx("div",{className:"p-2",children:"Let's build a map!"})};export{s as component};
--- assets/geocoder-CMUTT5Lo.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-vEIXQwmM.js";const e=function(){return o.jsx("div",{children:'Hello "/geocoder"!'})};export{e as component};
