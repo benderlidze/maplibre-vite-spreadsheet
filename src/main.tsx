56a3387b2e1db82eb5588f37ddb152fe23d513c6
Serg <benderlio@gmail.com>
2025-04-01 12:34:22 +0200

    github pages fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
+import {
+  RouterProvider,
+  createHashHistory,
+  createRouter,
+} from "@tanstack/react-router";
 import "./index.css";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 
-// Create a new router instance
 const router = createRouter({
   routeTree,
-  basepath: "",
-  defaultPreload: "intent",
+  basepath: "/",
+  history: createHashHistory(),
 });
 
 // Register the router instance for type safety
