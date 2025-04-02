cd945ffbf7710e572948a172ceb5c820791f56c3
Serg <benderlio@gmail.com>
2025-04-02 22:22:31 +0200

    Updates

---
+++ assets/about-Dnd2z9kb.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-EMo9WXU4.js";const e=function(){return o.jsx("div",{className:"p-2",children:"Free map"})};export{e as component};
--- assets/about-DvmEuWJw.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-BIyoVxsR.js";const e=function(){return o.jsx("div",{className:"p-2",children:"Free map"})};export{e as component};
