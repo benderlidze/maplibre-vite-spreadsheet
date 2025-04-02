40b8da7eaf341039c71147f9a9f8eab126804440
Serg <benderlio@gmail.com>
2025-04-02 16:33:18 +0200

    Updates

--- assets/about-DxNUO4H0.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-DiIIn6fF.js";const e=function(){return o.jsx("div",{className:"p-2",children:"Free map"})};export{e as component};
---
+++ assets/about-nzmQfcA1.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-Cp3jmcdL.js";const e=function(){return o.jsx("div",{className:"p-2",children:"Free map"})};export{e as component};
